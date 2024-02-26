4fbe5318bac0369a3c724a6dc62b5b8c6445b062
Yamil Casarreal <yamilcasarreal@gmail.com>
2024-02-26 10:35:41 -0800

    Initial Commit

---
+++ Location-Based-Coupons/Location_Based_CouponsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_Based_CouponsApp.swift
+//  Location-Based-Coupons
+//
+//  Created by Yamil Casarreal on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Location_Based_CouponsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
