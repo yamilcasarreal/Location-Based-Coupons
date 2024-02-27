0b76479811d63790c9ccfd7ded790227a3978db2
Yamil Casarreal <yamilcasarreal@gmail.com>
2024-02-27 15:48:29 -0500

    Registration form test

--- Location-Based-Coupons/Core/Authentication/LoginView.swift
+++ Location-Based-Coupons/Core/Authentication/LoginView.swift
@@ -40,7 +40,8 @@ struct LoginView: View {
                 
                 //sign up button
                 NavigationLink{
-                    
+                    RegistrationView()
+                        .navigationBarBackButtonHidden(true)
                 } label: {
                     HStack(spacing: 3){
                         Text("Don't have an account?")
