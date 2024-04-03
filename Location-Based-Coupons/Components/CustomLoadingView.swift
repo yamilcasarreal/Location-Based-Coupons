e48f726f0b1aa4ab7690099ad5b0d6d0ab343a7b
Yamil Casarreal <yamilcasarreal@gmail.com>
2024-04-03 12:41:23 -0400

    Added error messages and loading icons. User can reset their password

---
+++ Location-Based-Coupons/Components/CustomLoadingView.swift
@@ -0,0 +1,24 @@
+//
+//  CustomLoadingView.swift
+//  Location-Based-Coupons
+//
+//  Created by Yamil Casarreal on 4/3/24.
+//
+
+import SwiftUI
+
+struct CustomProgressView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(.circular)
+            .accentColor(.white)
+            .scaleEffect(x: 1.5, y: 1.5, anchor: .center)
+            .frame(width: 80, height: 80)
+            .background(Color(.systemGray4))
+            .cornerRadius(20)
+    }
+}
+
+#Preview {
+    CustomProgressView()
+}
--- Location-Based-Coupons/Core/Authentication/View/LoginView.swift
+++ Location-Based-Coupons/Core/Authentication/View/LoginView.swift
@@ -14,60 +14,85 @@ struct LoginView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                //image
-                Image("coupON2")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 100, height: 200)
-                    .padding(.vertical, 32)
-                //form fields
-                VStack (spacing: 24) {
-                    InputView(text: $email, 
-                              title: "Email Address",
-                              placeholder: "name@email.com")
-                    .autocapitalization(.none)
+            ZStack {
+                VStack {
+                    //image
+                    Image("coupON2")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 200)
+                        .padding(.vertical, 32)
+                    //form fields
+                    VStack (spacing: 24) {
+                        InputView(text: $email,
+                                  title: "Email Address",
+                                  placeholder: "name@email.com")
+                        .autocapitalization(.none)
+                        
+                        InputView(text: $password,
+                                  title: "Password", placeholder: "Enter your password",
+                                  isSecureField: true)
+                        .autocapitalization(.none)
+                        
+                        NavigationLink {
+                            ResetPasswordView()
+                                .navigationBarHidden(true)
+                        } label: {
+                            Text("Forgot Password?")
+                                .font(.system(size: 13, weight: .semibold))
+                                .foregroundColor(.green)
+                            
+                        }
+                        .padding(.bottom)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        
+                    }
                     
-                    InputView(text: $password, 
-                              title: "Password", placeholder: "Enter your password",
-                              isSecureField: true)
+                    .padding(.horizontal)
+                    .padding(.top, 12)
+                    
+                    //sign in button
+                    Button {
+                        Task {
+                            try await viewModel.signIn(withEmail: email, password: password)
+                        }
+                    } label: {
+                        HStack {
+                            Text("SIGN IN")
+                                .fontWeight(.semibold)
+                            Image(systemName: "arrow.right")
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                    }
+                    .disabled(!formIsValid)
+                    .opacity(formIsValid ? 1.0 : 0.5)
+                    .background(Color(.systemGreen))
+                    .cornerRadius(30)
+                    .padding(.top, 24)
+                    Spacer()
+                    
+                    //sign up button
+                    NavigationLink{
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack(spacing: 3){
+                            Text("Don't have an account?")
+                            Text("Sign Up")
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        }
+                        .font(.system(size: 15))
+                        .foregroundColor(.green)
+                    }
                 }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                
-                //sign in button
-                Button {
-                    Task {
-                        try await viewModel.signIn(withEmail: email, password: password)
-                    }
-                } label: {
-                    HStack {
-                        Text("SIGN IN")
-                            .fontWeight(.semibold)
-                        Image(systemName: "arrow.right")
-                    }
-                    .foregroundColor(.white)
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                .alert(isPresented: $viewModel.showAlert) {
+                    Alert(title: Text("Error"),
+                          message: Text(viewModel.authError?.description ?? ""))
                 }
-                .disabled(!formIsValid)
-                .opacity(formIsValid ? 1.0 : 0.5)
-                .background(Color(.systemGreen))
-                .cornerRadius(30)
-                .padding(.top, 24)
-                Spacer()
                 
-                //sign up button
-                NavigationLink{
-                    RegistrationView()
-                        .navigationBarBackButtonHidden(true)
-                } label: {
-                    HStack(spacing: 3){
-                        Text("Don't have an account?")
-                        Text("Sign Up")
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    }
-                    .font(.system(size: 15))
-                    .foregroundColor(.green)
+                if viewModel.isLoading {
+                    CustomProgressView()
                 }
             }
         }
